e8bf6233097fcf7612a7b1a0302fba4c9aba8123
Vladislaw <venusversusviruses@gmail.com>
2022-10-10 21:24:52 +0300

    services added

--- src/main/java/service/impl/user/impl/UserServiceImpl.java
+++ src/main/java/service/impl/user/impl/UserServiceImpl.java
@@ -1,4 +1,35 @@
 package service.impl.user.impl;
 
-public class UserServiceImpl {
+import persistence.datatable.DataTableRequest;
+import persistence.datatable.DataTableResponse;
+import persistence.entity.user.BaseUser;
+import service.impl.user.UserService;
+
+public class UserServiceImpl implements UserService {
+
+
+    @Override
+    public long create(BaseUser entity) {
+        return 0;
+    }
+
+    @Override
+    public boolean update(BaseUser entity) {
+        return false;
+    }
+
+    @Override
+    public boolean delete(Long id) {
+        return false;
+    }
+
+    @Override
+    public BaseUser findById(Long id) {
+        return null;
+    }
+
+    @Override
+    public DataTableResponse<BaseUser> findAll(DataTableRequest request) {
+        return null;
+    }
 }
