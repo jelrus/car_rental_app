6c686bafa946d14682007033ed035c2c49327742
Vladislaw <venusversusviruses@gmail.com>
2023-01-12 00:49:28 +0200

    final commit

---
+++ src/main/resources/sql/data.sql
@@ -0,0 +1,58 @@
+insert into users values(1, '2022-11-01 01:08:27', '2023-01-11 18:53:59.415000', 'admin', 'admin',
+                         'adminName', 'adminLastName', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRI3sKSxzwYoqOeReznBVgroQ2zM2FnZ29x3Q&usqp=CAU',
+                         '227600.00', 'Admin description', 1, 'ROLE_ADMIN');
+insert into users values(2, '2022-11-03 12:45:57.989000', '2022-11-03 12:45:57.989000', 'user', 'user',
+                         'userName', 'userLastName', 'https://www.shutterstock.com/blog/wp-content/uploads/sites/5/2019/07/Man-Silhouette.jpg',
+                         '2000.00','User description',1,'ROLE_USER');
+insert into users values(3, '2022-11-03 12:48:03.135000', '2022-12-22 11:19:45.073000', 'manager', 'manager',
+                         'managerName', 'managerLastName',' https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460__480.png',
+                         '8700.00', 'Manager description', 1, 'ROLE_MANAGER');
+
+insert into cars values(1, '2022-11-03 12:45:57.989000', '2022-11-03 12:45:57.989000', 'BMW MPV 2015',
+                        'https://mediacloud.carbuyer.co.uk/image/private/s--HPh_zYtq--/v1584466291/carbuyer/car_images/bmw2seriesgtcutout.jpg', 'BMW', 'MPV',
+                        'Product description', '200.00', 1);
+insert into cars values(2, '2022-11-03 12:45:57.989000', '2022-11-03 12:45:57.989000', 'BMW Luxury 2014',
+                        'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRUcWqnNK9oIz2yPXysxx_vCQ0oD1cMpQNGRw&usqp=CAU', 'BMW', 'LUXURY',
+                        'Product description', '250.00', 1);
+insert into cars values(3, '2022-11-03 12:45:57.989000', '2022-11-03 12:45:57.989000', 'BMW Sports 2016',
+                        'https://imgd.aeplcdn.com/0x0/cw/ec/37095/BMW-Z4-Roadster-Right-Front-Three-Quarter-153914.jpg?wm=0', 'BMW', 'SPORTS',
+                        'Product description', '240.00', 1);
+insert into cars values(4, '2022-11-03 12:45:57.989000', '2022-11-03 12:45:57.989000', 'BMW SUV 2015',
+                        'https://cdn.hevcars.com.ua/wp-content/uploads/2018/09/bmw-x5-xdrive45e-iperformance-exterior-hevcars-01.jpg', 'BMW', 'SUV',
+                        'Product description', '210.00', 1);
+insert into cars values(5, '2022-11-03 12:45:57.989000', '2022-11-03 12:45:57.989000', 'MERCEDES MPV 2012',
+                        'https://mediacloud.carbuyer.co.uk/image/private/s--h8Z0cjJP--/v1584466023/carbuyer/car_images/mercedes-vclass-mpv-cutout-2019.jpg', 'MERCEDES', 'MPV',
+                        'Product description', '190.00', 1);
+insert into cars values(6, '2022-11-03 12:45:57.989000', '2022-11-03 12:45:57.989000', 'MERCEDES LUXURY 2014',
+                        'https://cars.usnews.com/static/images/Auto/izmo/i159614957/2022_mercedes_benz_s_class_angularfront.jpg', 'MERCEDES', 'LUXURY',
+                        'Product description', '220.00', 1);
+insert into cars values(7, '2022-11-03 12:45:57.989000', '2022-11-03 12:45:57.989000', 'MERCEDES Sports 2013',
+                        'https://imgd.aeplcdn.com/0x0/n/cw/ec/20865/amg-gt-exterior-right-front-three-quarter-60800.jpeg', 'MERCEDES', 'SPORTS',
+                        'Product description', '235.00', 1);
+insert into cars values(8, '2022-11-03 12:45:57.989000', '2022-11-03 12:45:57.989000', 'MERCEDES SUV 2012',
+                        'https://s3.us-east-2.amazonaws.com/dealer-inspire-vps-vehicle-images/stock-images/thumbnails/large/chrome/992ae4b97cf88e5f50cd5fe3e87372cf.png', 'MERCEDES', 'SUV',
+                        'Product description', '225.00', 1);
+insert into cars values(9, '2022-11-03 12:45:57.989000', '2022-11-03 12:45:57.989000', 'HONDA MPV 2013',
+                        'http://www.autoconsulting.com.ua/pictures/Honda/2013/Honda_ConceptMPV_02.jpg', 'HONDA', 'MPV',
+                        'Product description', '150.00', 1);
+insert into cars values(10, '2022-11-03 12:45:57.989000', '2022-11-03 12:45:57.989000', 'HONDA Luxury 2017',
+                        'https://imgd.aeplcdn.com/664x374/cw/ec/21613/Honda-Accord-Right-Front-Three-Quarter-82683.jpg?v=201711021421&q=75', 'HONDA', 'LUXURY',
+                        'Product description', '180.00', 1);
+insert into cars values(11, '2022-11-03 12:45:57.989000', '2022-11-03 12:45:57.989000', 'HONDA Sports 2018',
+                        'https://www.autocar.co.uk/sites/autocar.co.uk/files/honda-nsx_2.jpg', 'HONDA', 'SPORTS',
+                        'Product description', '190.00', 1);
+insert into cars values(12, '2022-11-03 12:45:57.989000', '2022-11-03 12:45:57.989000', 'HONDA SUV 2017',
+                        'https://itc.ua/wp-content/uploads/2020/09/honda-suv-econcept-1.jpg', 'HONDA', 'SUV',
+                        'Product description', '170.00', 1);
+insert into cars values(13, '2022-11-03 12:45:57.989000', '2022-11-03 12:45:57.989000', 'TOYOTA MPV 2015',
+                        'https://imgd.aeplcdn.com/1200x900/n/cw/ec/41217/toyota-vellfire-right-front-three-quarter5.jpeg?q=75', 'TOYOTA', 'MPV',
+                        'Product description', '235.00', 1);
+insert into cars values(14, '2022-11-03 12:45:57.989000', '2022-11-03 12:45:57.989000', 'TOYOTA Luxury 2018',
+                        'https://cars.usnews.com/static/images/Auto/izmo/i159614732/2022_toyota_gr_supra_angularfront.jpg', 'TOYOTA', 'LUXURY',
+                        'Product description', '230.00', 1);
+insert into cars values(15, '2022-11-03 12:45:57.989000', '2022-11-03 12:45:57.989000', 'TOYOTA Sports 2016',
+                        'https://autoreview.ru/images/Article/1654/Article_165438_860_575.jpg', 'TOYOTA', 'SPORTS',
+                        'Product description', '205.00', 1);
+insert into cars values(16, '2022-11-03 12:45:57.989000', '2022-11-03 12:45:57.989000', 'TOYOTA SUV 2020',
+                        'https://etimg.etb2bimg.com/photo/86836484.cms', 'TOYOTA', 'SUV',
+                        'Product description', '250.00', 1);
